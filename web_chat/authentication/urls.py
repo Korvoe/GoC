1c1155e5d194396a90101d7d853b61fd2ea45ba2
Korvoe <indestructible228@gmail.com>
2019-10-30 18:28:08 +0900

    Possible to see users

--- web_chat/authentication/urls.py
+++ web_chat/authentication/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('registration/', views.Registration.as_view(), name='registration'),
     path('', include('django.contrib.auth.urls')),
+    path('', views.HomeView.as_view(), name='home'),
 ]
--- web_chat/authentication/views.py
+++ web_chat/authentication/views.py
@@ -1,9 +1,19 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+from django.views.generic import TemplateView
+from django.shortcuts import render
 # Create your views here.
 
 class Registration(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/registration.html'
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+    def get(self, request):
+        users = CustomUser.objects.all()
+        args = {'users': users}
+        return render(request, self.template_name, args)
