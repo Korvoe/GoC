12edb420a13d01425298cb4639b8cc5ff8d5991d
Korvoe <indestructible228@gmail.com>
2019-11-17 16:27:02 +0900

    Fixed the access to rooms

--- web_chat/chat/views.py
+++ web_chat/chat/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.utils.safestring import mark_safe
 from django.contrib.auth.decorators import login_required
+from chat.models import Thread
 import json
 
 # Create your views here.
 
 @login_required
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)),
-        'username': mark_safe(json.dumps(request.user.username)),
-    })
+    if Thread.objects.filter(room_id=room_name).exists():
+        return render(request, 'chat/room.html', {
+            'room_name_json': mark_safe(json.dumps(room_name)),
+            'username': mark_safe(json.dumps(request.user.username)),
+            })
+    else:
+        raise Http404("Page not fould")
