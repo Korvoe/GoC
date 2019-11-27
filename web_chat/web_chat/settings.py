f9e1a865fda8a419bc3006cdf095c230ca8ce583
Korvoe <indestructible228@gmail.com>
2019-11-27 15:44:51 +0900

    Removed bugs

--- web_chat/web_chat/settings.py
+++ web_chat/web_chat/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
-    'encrypted_model_fields',
     'channels',
     'chat',
     ]
